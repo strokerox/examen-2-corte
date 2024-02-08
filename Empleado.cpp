3e280690048d2a938c647e6561ced1c8a401d149
strokerox <148830055+strokerox@users.noreply.github.com>
2024-02-08 19:09:09 -0400

    Empleado.cpp

--- Empleado.cpp
+++ Empleado.cpp
@@ -23,6 +23,7 @@ private:
     double Deducciones;
 
 public:
+//construtor del empleado 
     Empleado(int cedula,string nombre, string apellido, int edad, int hijos, double sueldo, string cargo, int anos, int dias) {
         Cedula = cedula;
         Nombre = nombre;
@@ -42,16 +43,18 @@ public:
     }
 
     void MostrarDatos() {
+        //imprimir la información 
         cout << "Asignaciones: $" << Asignaciones << std::endl;
         cout << "Deducciones: $" << Deducciones << std::endl;
         cout << "Sueldo Mensual: Bs. " << fixed << setprecision(2) << SueldoMensual << " (Tasa de cambio oficial BCV)\n";
-    }
-};
+    }//fin del metodo calcular sueldo
+};//fin de la clase empleado 
 
 int main() {
+    //insertamos un objeto 
     Empleado empleado(1234567, "Juan", "Perez", 30, 2, 20.0, "Gerente", 5, 22);
     empleado.CalcularSueldoMensual();
     empleado.MostrarDatos();
 
     return 0;
-}
+}//fin del programa 
